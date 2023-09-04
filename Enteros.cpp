56abff798fcb6831482abbb485aac7e0b14a1c47
Esteban2135 <142956071+Esteban2135@users.noreply.github.com>
2023-09-04 12:47:40 -0500

    Parciales

---
+++ Enteros.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+    int entero[] (1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,28,30)=;
+        int m=30;
+        int i,j,aux;
+        
+        for (i=1;i<n;i++){
+            negativo = false ;
+            for (i=n-1;j>=i;j--)
+            if (entero [j-1]);
+            aux = entero [j-1];
+            entero [j-1]=entero[j];
+        }
+    return 0;
+}
